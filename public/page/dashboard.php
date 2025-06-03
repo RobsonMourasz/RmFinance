460c9b890bc239459ffe7b4372ecf983728a1b63
RobsonMourasz <robsonic10@gmail.com>
2025-06-03 18:47:38 -0300

    adicionar tabela de vencimentos e atualizar informações das movimentações no dashboard

--- public/page/dashboard.php
+++ public/page/dashboard.php
@@ -33,20 +33,21 @@
     </div> <!-- card financeiro -->
 
     <div class="card">
-        <div class="card-movimentacao bg-primary">
+
+    <div class="card-movimentacao bg-primary">
             <div class="card-title">
                 <div class="title">
                     <i class="bi bi-graph-up-arrow"></i>
-                    <h2>Movimentações</h2>
+                    <h2>Á vencer</h2>
                 </div>
                 <div class="sub-title">
-                    <p>Últimas movimentações do sistema</p>
+                    <p>Próximos vencimentos</p>
                 </div>
 
             </div>
             <div class="card-body">
                 <div class="tabela">
-                    
+
                     <div class="cabecalho">
                         <div class="linha">
                             <div class="col">Data</div>
@@ -88,7 +89,66 @@
 
             </div> <!-- card body -->
 
-        </div> <!-- card movimentacao -->
+        </div> <!-- card  -->
 
-    </div> <!-- card movimentacoes -->
+        <div class="card-movimentacao bg-primary">
+            <div class="card-title">
+                <div class="title">
+                    <i class="bi bi-graph-up-arrow"></i>
+                    <h2>Movimentações</h2>
+                </div>
+                <div class="sub-title">
+                    <p>5 Últimos lancamentos do sistema</p>
+                </div>
+
+            </div>
+            <div class="card-body">
+                <div class="tabela">
+
+                    <div class="cabecalho">
+                        <div class="linha">
+                            <div class="col">Data</div>
+                            <div class="col">Descrição</div>
+                            <div class="col">Valor</div>
+                        </div>
+
+                    </div> <!-- cabeçalho da tabela -->
+
+                    <div class="corpo">
+
+                        <div class="linha">
+                            <div class="row">10/12/1997</div>
+                            <div class="row">Agua</div>
+                            <div class="row">R$ 12,00</div>
+                        </div>
+
+                        <div class="linha">
+                            <div class="row">10/12/1997</div>
+                            <div class="row">Agua</div>
+                            <div class="row">R$ 12,00</div>
+                        </div>
+
+                        <div class="linha">
+                            <div class="row">10/12/1997</div>
+                            <div class="row">Agua</div>
+                            <div class="row">R$ 12,00</div>
+                        </div>
+
+                        <div class="linha">
+                            <div class="row">10/12/1997</div>
+                            <div class="row">Agua</div>
+                            <div class="row">R$ 12,00</div>
+                        </div>
+
+                    </div> <!-- corpo da tabela -->
+
+                </div> <!-- tabela -->
+
+            </div> <!-- card body -->
+
+        </div> <!-- card  -->
+
+    </div> <!-- FIM card movimentacoes -->
+
+    
 </div>
