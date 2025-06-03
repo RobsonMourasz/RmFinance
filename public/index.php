88a3482783907a8e287c82a40101d780b32daf0f
RobsonMourasz <robsonic10@gmail.com>
2025-06-03 12:29:46 -0300

    refatorar layout e estilos do cabeçalho, navegação e rodapé; adicionar transições ao menu

--- public/index.php
+++ public/index.php
@@ -18,49 +18,56 @@
         </div> <!-- Logo do site -->
     </header>
 
-    <nav class="">
-        <ul>
+    <main>
 
-            <li><a href="#"> <i class="bi bi-graph-up-arrow"></i> Home</a></li>
-            <li><a href="#"> <i class="bi bi-building-fill-down"></i> Despesas</a></li>
-            <li><a href="#"> <i class="bi bi-building-add"></i> Receitas</a></li>
-            <li><a href="#"> <i class="bi bi-file-earmark-bar-graph"></i> Relatórios</a></li>
-            <li><a href="#"> <i class="bi bi-indent"></i> Logoff</a></li>
+        <nav class="">
+            <ul>
 
-        </ul>
-    </nav>
+                <li><a href="#"> <i class="bi bi-graph-up-arrow"></i> Home</a></li>
+                <li><a href="#"> <i class="bi bi-building-fill-down"></i> Despesas</a></li>
+                <li><a href="#"> <i class="bi bi-building-add"></i> Receitas</a></li>
+                <li><a href="#"> <i class="bi bi-file-earmark-bar-graph"></i> Relatórios</a></li>
+                <li><a href="#"> <i class="bi bi-indent"></i> Logoff</a></li>
+
+            </ul>
+        </nav>
+        <section>
+            
+        </section>
+    </main>
 
     <footer>
-
+        Sistema criado por Robson Moura &copy; 2023
     </footer>
 
     <script>
         const toggle = document.querySelector('.toggle');
         const nav = document.querySelector('nav');
 
-        toggle.addEventListener('click', function () {
+        toggle.addEventListener('click', function() {
             nav.classList.toggle('nav-toggle');
         });
 
         // Fecha o menu ao clicar fora dele
-        document.addEventListener('click', function (event) {
-            if (!nav.contains(event.target) && !toggle.contains(event.target)) {
-                nav.classList.remove('nav-toggle');
-            }
-        });
+
 
         if (window.innerWidth <= 768) {
             nav.classList.add('nav-toggle');
+
+            document.addEventListener('click', function(event) {
+                if (!nav.contains(event.target) && !toggle.contains(event.target)) {
+                    nav.classList.add('nav-toggle');
+                }
+            });
         }
 
-        window.addEventListener('resize', function () {
+        window.addEventListener('resize', function() {
             if (window.innerWidth <= 768) {
                 nav.classList.add('nav-toggle');
             } else {
                 nav.classList.remove('nav-toggle');
             }
         });
-        
     </script>
 
 </body>
